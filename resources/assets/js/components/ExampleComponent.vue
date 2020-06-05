4a513795eb5ea4000013c43224eb009b4a6d3677
Kun8018 <1027690173@qq.com>
2020-06-05 15:17:04 +0800

    laravel-vue

---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>   
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                visible:false
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
